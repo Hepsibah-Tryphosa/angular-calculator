22bc136c022540d8f9aa750685f28ebb8b65da41
hepsibah <hepsibahchitturi@gmail.com>
2023-08-16 13:19:38 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,21 @@ export class AppComponent {
   calValue: number = 0;
   funcT: any = 'NoFunction';
   calNumber: string = 'noValue';
+  firstNumber: number = 0;
+  secondNumber: number = 0;
 
+  
   onClickValue(val: string, type: any) {
     if (type == 'number') {
       this.onNumberClick(val);
 
     }
+    else if (type == 'function') {
+      this.onFunctionClick(val);
+    }
   }
   onNumberClick(val: string) {
-    if (this.calNumber !='noValue') {
+    if (this.calNumber != 'noValue') {
       this.calNumber = this.calNumber + val;
 
     }
@@ -28,5 +34,73 @@ export class AppComponent {
       this.calNumber = val;
     }
     this.calValue = parseFloat(this.calNumber);
+  }
+  onFunctionClick(val: string) {
+    if (val == 'c') {
+      this.clearClick();
+    }
+    else if (this.funcT == 'NoFunction') {
+      this.firstNumber = this.calValue;
+      this.calValue = 0;
+      this.funcT = val;
+      this.calNumber = 'noValue';
+    }
+    else if (this.funcT != 'NoFunction') {
+      this.secondNumber = this.calValue;
+      this.valCalculate(val);
+    }
+  }
+  valCalculate(val: string) {
+    console.log(this.funcT);
+    if (this.funcT == '+') {
+      const Total = this.firstNumber + this.secondNumber
+      this.totalAssignVal(Total, val);
+
+    }
+    if (this.funcT == '-') {
+      const Total = this.firstNumber + this.secondNumber
+      this.totalAssignVal(Total, val);
+
+    }
+    if (this.funcT == '*') {
+      const Total = this.firstNumber + this.secondNumber
+      this.totalAssignVal(Total, val);
+
+    }
+    if (this.funcT == '%') {
+      const Total = this.firstNumber + this.secondNumber
+      this.totalAssignVal(Total, val);
+
+    }
+    if (this.funcT == '/') {
+      const Total = this.firstNumber + this.secondNumber
+      this.totalAssignVal(Total, val);
+
+    }
+
+
+  }
+  totalAssignVal(Total: number, val: string) {
+    this.calValue = Total;
+    this.firstNumber = Total;
+    this.secondNumber = 0;
+    this.funcT = 'val';
+    this.calNumber = 'noValue';
+    if (val == '=') {
+      this.equalClick();
+    }
+  }
+  equalClick() {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.funcT = 'NoFunction';
+    this.calNumber = 'noValue';
+  }
+  clearClick() {
+    this.calValue = 0;
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.funcT = 'NoFunction';
+    this.calNumber = 'noValue';
   }
 }
